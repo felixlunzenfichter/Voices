913eb8b06c3d7997eba101df1ba0206c1d281285
Felix Lunzenfichter <felixlunzenfichter@gmail.com>
2020-10-07 13:04:28 +0200

    First commit.

--- Voices/VoicesApp.swift
+++ Voices/VoicesApp.swift
@@ -18,3 +18,9 @@ struct VoicesApp: App {
         }
     }
 }
+
+struct VoicesApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
