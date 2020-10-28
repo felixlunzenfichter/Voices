f70b45dc8f0e47901d62e6d9de8e9828d0b44698
Felix Lunzenfichter <felixlunzenfichter@gmail.com>
2020-10-28 14:51:02 +0100

    Don't reset player when transcription is ready. Refactor Listening view.

--- Voices/View/ListeningView.swift
+++ Voices/View/ListeningView.swift
@@ -11,27 +11,85 @@ import AVFoundation
 struct ListeningView: View {
         
     private var voice : Voice
-    var audioPlayer : AudioPlayer
     
     init(voice: Voice) {
         print("init Listeningview")
         self.voice = voice
-        self.audioPlayer = AudioPlayer(voice: voice)
     }
     
     var body: some View {
         VStack {
-            HStack(alignment: .center) {
-                TranscriptionView(voice: voice)
-                Spacer()
-                LanguagePicker(voice: voice)
-            }
-            MySlider(audioPlayer: audioPlayer)
-            PlayerControls(audioPlayer: audioPlayer)
+            TextualRepresentationSection(voice: voice)
+            AudioSection(voice: voice)
         }
     }
 }
 
+struct TextualRepresentationSection : View {
+    
+    var voice : Voice
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            TranscriptionView(voice: voice)
+            Spacer()
+            LanguagePicker(voice: voice)
+        }
+    }
+}
+
+struct AudioSection : View {
+    var voice : Voice
+    var audioPlayer : AudioPlayer
+    
+    init(voice: Voice) {
+        self.voice = voice
+        audioPlayer = AudioPlayer(voice: voice)
+    }
+    
+    var body: some View {
+        MySlider(audioPlayer: audioPlayer)
+        PlayerControls(audioPlayer: audioPlayer)
+    }
+}
+
+struct TranscriptionView : View {
+    
+    var voice: Voice
+     
+    var body: some View {
+        buildTranscriptionView(voice: voice)
+    }
+
+}
+
+func buildTranscriptionView(voice: Voice) -> some View {
+    
+    let speechToText = SpeechToText()
+    return TranscriptionViewContentView(speechToText: speechToText, voice: voice)
+}
+
+struct TranscriptionViewContentView : View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    @ObservedObject var speechToText : SpeechToText
+    var voice : Voice
+    
+    var body: some View {
+        HStack {
+            if (speechToText.isTranscribing) {
+                ProgressView("transcribing").padding()
+            } else {
+                HStack{
+                    Button(action: {speechToText.transcribe(voice: voice, viewContext: viewContext)}, label: {
+                        Text("transcribe").lineLimit(0).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    }).padding()
+                    Text(voice.transcript ?? "error no transcript found.")
+                }
+            }
+        }.frame(height: 100)
+    }
+}
 
 struct LanguagePicker : View {
     
@@ -71,51 +129,9 @@ struct LanguagePicker : View {
     }
 }
 
-func buildTranscriptionView(voice: Voice) -> some View {
-    
-    let speechToText = SpeechToText()
-    return TranscriptionViewContentView(speechToText: speechToText, voice: voice)
-}
-
-struct TranscriptionView : View {
-    
-    @ObservedObject private var voice: Voice
- 
-    public init(voice: Voice) {
-        self.voice = voice
-    }
-    
-    var body: some View {
-        buildTranscriptionView(voice: voice)
-    }
-
-}
-
-struct PlayButton : View {
-    
-    @ObservedObject private var audioPlayer : AudioPlayer
-    
-    public init (audioPlayer: AudioPlayer) {
-        self.audioPlayer = audioPlayer
-    }
-    
-    var body: some View {
-        Button(
-            action:{
-                audioPlayer.isListening ? audioPlayer.pause() : audioPlayer.play()
-        },  label: {
-            Image(systemName: audioPlayer.isListening ? "pause" : "play" ).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        })
-    }
-}
-
 struct PlayerControls : View {
     
-    private var audioPlayer : AudioPlayer
-    
-    public init(audioPlayer:  AudioPlayer) {
-        self.audioPlayer = audioPlayer
-    }
+    var audioPlayer : AudioPlayer
     
     var body: some View {
         return HStack {
@@ -134,27 +150,25 @@ struct PlayerControls : View {
     }
 }
 
-struct TranscriptionViewContentView : View {
+struct PlayButton : View {
     
-    @Environment(\.managedObjectContext) private var viewContext
-    @ObservedObject var speechToText : SpeechToText
-    var voice : Voice
+    @ObservedObject private var audioPlayer : AudioPlayer
+    
+    public init (audioPlayer: AudioPlayer) {
+        print("init playButton")
+        self.audioPlayer = audioPlayer
+    }
     
     var body: some View {
-        HStack {
-            if (speechToText.isTranscribing) {
-                ProgressView("transcribing").padding()
-            } else {
-                HStack{
-                    Button(action: {speechToText.transcribe(voice: voice, viewContext: viewContext)}, label: {
-                        Text("transcribe").lineLimit(0).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    }).padding()
-                    Text(voice.transcript ?? "error no transcript found.")
-                }
-            }
-        }.frame(height: 100)
+        Button(
+            action:{
+                audioPlayer.isListening ? audioPlayer.pause() : audioPlayer.play()
+        },  label: {
+            Image(systemName: audioPlayer.isListening ? "pause" : "play" ).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        })
     }
 }
+
 
 struct ListeningView_Previews: PreviewProvider {
     static var previews: some View {
