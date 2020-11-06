b7eb7e0ce744b37b2d810fa1400fe2432e16b76e
Felix Lunzenfichter <felixlunzenfichter@gmail.com>
2020-11-06 04:06:35 +0100

    Record and stop recording UI.

---
+++ Voices/Controller/Audio.swift
@@ -0,0 +1,16 @@
+//
+// Created by Felix Lunzenfichter on 06.11.20.
+//
+
+import Foundation
+
+class Audio {
+
+    func startRecording() {
+        
+    }
+
+    func stopRecording() {
+
+    }
+}
--- Voices/View/ConversationView.swift
+++ Voices/View/ConversationView.swift
@@ -11,32 +11,33 @@ import AVFoundation
 struct ConversationView: View {
     
     let voices: [Voice]
-    var audioSession: AVAudioSession!
-    var voiceRecorder: AVAudioRecorder!
-    
-    init(voices: [Voice]) {
-        self.voices = voices
+    let audio: Audio = Audio()
+    @State var isRecording = false
 
-        audioSession = AVAudioSession.sharedInstance()
-
-        do {
-            try audioSession.setCategory(.playAndRecord, mode: .default)
-        } catch {
-            print(error)
-        }
-
-        
-    }
-    
     var body: some View {
         VStack {
             List (voices) { voice in
                 VoiceView(voice: voice)
             }
             Spacer()
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Image(systemName: "music.mic").font(.system(size: 200))
-            })
+            if (!isRecording) {
+                Button(action: {audio.startRecording(); isRecording.toggle()}, label: {
+                    Image(systemName: "music.mic").font(.system(size: 200))
+                })
+            } else {
+                Button(
+                    action: {
+                        isRecording.toggle()
+                        audio.stopRecording()
+//                      CloudKitInterface.send()
+                        return
+                    },
+                    label: {
+                        Image(systemName: "stop.circle").font(.system(size: 200))
+                    })
+            }
+
+
             
         }
     }
